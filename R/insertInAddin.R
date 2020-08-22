f48fc784fedac68e6e8890dacc573febd99c0060
WooJin1993 <ahnwj2004@gmail.com>
2020-08-22 15:11:38 +0900

    make 'inserInAddin' function

---
+++ R/insertInAddin.R
@@ -0,0 +1,9 @@
+#' Insert \%\<\>\%.
+#'
+#' Call this function as an addin to insert \code{ \%\<\>\% } at the cursor position.
+#'
+#' @export
+
+insertInAddin <- function() {
+  rstudioapi::insertText(" %<>% ")
+}
